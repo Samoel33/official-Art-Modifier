34724d59865ec234c5ebf947e0cdebe8dd6ae297
Samoel33 <68919054+Samoel33@users.noreply.github.com>
2021-07-30 11:47:21 +0200

    rectify

--- public/js/index.js
+++ public/js/index.js
@@ -6,32 +6,32 @@ const lists = document.querySelector(".aboutArtM").href;
 const glass = document.querySelector(".glass");
 
 blog_message.addEventListener("click", () => {
-	if (lists == "http://localhost:5000/about-art") {
-		console.log("lets see");
-		glass.style.width = "100%";
-		glass.style.transform = "translate(0) rotate(0deg)";
-		glass.style.opacity = "1";
-	}
-	setTimeout(() => {
-		showMessage.classList.remove("showMessage");
-	}, 3000);
-	showMessage.classList.toggle("showMessage");
+    if (lists == "/about-art") {
+        console.log("lets see");
+        glass.style.width = "100%";
+        glass.style.transform = "translate(0) rotate(0deg)";
+        glass.style.opacity = "1";
+    }
+    setTimeout(() => {
+        showMessage.classList.remove("showMessage");
+    }, 3000);
+    showMessage.classList.toggle("showMessage");
 });
 clothing_message.addEventListener("click", () => {
-	if (lists == "http://localhost:5000/about-art") {
-		console.log("lets see");
-		glass.style.width = "100%";
-		glass.style.transform = "translate(0) rotate(0deg)";
-		glass.style.opacity = "1";
-	}
-	setTimeout(() => {
-		showMessage.classList.remove("showMessage");
-	}, 3000);
-	showMessage.classList.toggle("showMessage");
+    if (lists == "/about-art") {
+        console.log("lets see");
+        glass.style.width = "100%";
+        glass.style.transform = "translate(0) rotate(0deg)";
+        glass.style.opacity = "1";
+    }
+    setTimeout(() => {
+        showMessage.classList.remove("showMessage");
+    }, 3000);
+    showMessage.classList.toggle("showMessage");
 });
-if (lists == "http://localhost:5000/about-art") {
-	console.log("lets see");
-	glass.style.width = "100%";
-	glass.style.transform = "translate(0) rotate(0deg)";
-	glass.style.opacity = "1";
+if (lists == "/about-art") {
+    console.log("lets see");
+    glass.style.width = "100%";
+    glass.style.transform = "translate(0) rotate(0deg)";
+    glass.style.opacity = "1";
 }
